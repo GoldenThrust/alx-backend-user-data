3b1000eb8472e0a405bc9f5d5915969681ebd408
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-08 22:36:06 +0100

    Files

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Filtered Logger """
+import re
+import logging
+
+
+def filter_datum(fields, redaction, message, separator):
+    for field in fields:
+        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
