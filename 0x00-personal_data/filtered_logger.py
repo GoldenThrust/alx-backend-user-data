bdf91f9ac4c99c9d3ec7f47975eb4a8719a50604
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-09 01:22:52 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,11 +2,16 @@
 """ Filtered Logger """
 import re
 import logging
+from os import getenv
+from typing import List
+from mysql import connector
 
 
-def filter_datum(fields, redaction, message, separator):
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+def filter_datum(fields: List, redaction: str, message: str, separator: str) -> str:
     for field in fields:
-        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+        message = re.sub(f"{field}=[^{separator}]*", f"{field}={redaction}", message)
 
     return message
 
@@ -19,8 +24,27 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        string = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, string, self.SEPARATOR)
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger_streamhandler = logging.StreamHandler()
+    logger_streamhandler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+def get_db():
+    username = getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    hostname = getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    dbname = getenv("PERSONAL_DATA_DB_NAME")
+
+    return connector.connect(
+        user=username, password=password, host=hostname, database=dbname
+    )
