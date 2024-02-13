bcd382259e311461f0f61d3d0cbe2e7ed59c0215
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-13 23:49:04 +0100

    Path

--- 0x01-Basic_authentication/api/v1/views/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/views/auth/auth.py
@@ -6,6 +6,12 @@ from flask import request
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        if path is None or excluded_paths is None:
+            return True
+        
+        if path not in excluded_paths or f'{path}/' not in excluded_paths:
+            return True
+        
         return False
 
     def authorization_header(self, request=None) -> str:
