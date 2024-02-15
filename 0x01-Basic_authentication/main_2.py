9ea93c2b3774cf206632883248939852d52e513e
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-15 02:30:38 +0100

    Auth

--- 0x01-Basic_authentication/main_2.py
+++ 0x01-Basic_authentication/main_2.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
-""" Main 1
+""" Main 5
 """
-from api.v1.auth.auth import Auth
+import uuid
+from api.v1.auth.basic_auth import BasicAuth
+from models.user import User
 
-a = Auth()
+""" Create a user test """
+user = User()
+user.email = "u3@gmail.com"
+user.first_name = "Sponge"
+user.last_name = "Bob"
+user.password = "pwd"
+user.save()
+print("New user: {}".format(user.display_name()))
+user.save()
 
-print(a.require_auth(None, None))
-print(a.require_auth(None, []))
-print(a.require_auth("/api/v1/status/", []))
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
-print(a.require_auth("/api/v1/users", ["/api/v1/stat*"]))
-print(a.require_auth("/api/v1/status", ["/api/v1/stat*"]))
-print(a.require_auth("/api/v1/stats", ["/api/v1/stat*"]))
+user_email = str(uuid.uuid4())
+user_clear_pwd = str(uuid.uuid4())
+user = User()
+user.email = user_email
+user.first_name = "Bob"
+user.last_name = "Dylan"
+user.password = user_clear_pwd
+print("New user: {}".format(user.display_name()))
+user.save()
+
+""" Retreive this user via the class BasicAuth """
+
+a = BasicAuth()
+
+u = a.user_object_from_credentials(None, "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(333, "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("email", None)
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("email", 333)
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("u1@gmail.com", "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("u2@gmail.com", "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials("u3@gmail.com", "pwd")
+print(u.display_name() if u is not None else "None")
+
+u = a.user_object_from_credentials(user_email, user_clear_pwd)
+print(u.display_name() if u is not None else "None")
