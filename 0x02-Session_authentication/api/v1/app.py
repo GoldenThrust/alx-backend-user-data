e3317c059f85316538f25d081581b534a235a02a
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-17 23:09:09 +0100

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,10 +39,12 @@ def before_request():
         request.path, [
             "/api/v1/status/",
             "/api/v1/unauthorized/",
-            "/api/v1/forbidden/"
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/"
             ]
     ):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and \
+        auth.session_cookie(request) is None:
             abort(401)
         if user is None:
             abort(403)
