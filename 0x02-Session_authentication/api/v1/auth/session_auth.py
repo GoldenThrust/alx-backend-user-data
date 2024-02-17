c6cb8339141b43bf09cf2cc7000de1bfddae38a4
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-17 22:15:22 +0100

    Docs

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,9 +5,11 @@ import uuid
 from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
+    """ Session Authentication """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """ Create a session """
         if user_id is None or not isinstance(user_id, str):
             return None
 
