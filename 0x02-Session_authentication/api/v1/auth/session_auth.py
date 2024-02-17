40cd19f9581c255c4c4424782f98078a303371e8
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-17 23:25:07 +0100

    User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,7 +28,7 @@ class SessionAuth(Auth):
         return self.user_id_by_session_id.get(session_id)
 
     def current_user(self, request=None):
-        """Return the current user id"""
+        """Return the current user"""
         session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id.get(session_id)
+        user_id = self.user_id_for_session_id[session_id]
         return User.get(user_id)
