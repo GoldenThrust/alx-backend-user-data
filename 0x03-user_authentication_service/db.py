0c01d820e2a80ca27e236b8e7bd080af96fcece5
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-21 22:42:18 +0100

    Find User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -48,5 +49,21 @@ class DB:
         return user
     
     def find_user_by(self, **kwargs):
-        pass
-        
+        """_summary_
+
+        Raises:
+            InvalidRequestError: _description_
+            NoResultFound: _description_
+
+        Returns:
+            _type_: _description_
+        """
+        users = self._session.query(User)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise InvalidRequestError
+            
+            for user in users:
+                if getattr(user, key) == v:
+                    return user
+        raise NoResultFound
