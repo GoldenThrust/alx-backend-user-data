772b8d70e474bc03cde25762f28dd7667b729c33
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-22 23:45:22 +0100

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,5 +84,42 @@ def profile() -> str:
     return jsonify({"email": "{}".format(user.email)}), 200
 
 
+@app.route("/reset_password", methods=["POST"], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get("email")
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": "{}", "reset_token": "{}".format(
+        email, reset_token)})
+
+
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
+def update_password() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """
+    email = request.form.get("email")
+    password = request.form.get("new_password")
+    reset_token = request.form.get("reset_token")
+
+    try:
+        AUTH.update_password(reset_token, password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": "{}", "message": "Password updated".format(
+        email)})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
