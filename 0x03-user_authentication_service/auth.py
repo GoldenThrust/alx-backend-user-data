cb5d5895786716f297772b2424e24b4b8e1ca467
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-22 16:04:00 +0100

    Eagle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 
+
 def _hash_password(password: str) -> bytes:
     """_summary_
 
@@ -14,8 +15,6 @@ def _hash_password(password: str) -> bytes:
         bytes: _description_
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-
-
 
 
 class Auth:
